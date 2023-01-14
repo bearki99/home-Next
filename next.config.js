8491bffc2e59ba140f09dba56b796c31ea7feb56
j10c <blyb1739@gmail.com>
2023-01-14 11:40:03 +0800

    feat(*): initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const withLess = require("next-with-less");
+const UnoCSS = require("@unocss/webpack").default;
+const presetUno = require("@unocss/preset-uno").default;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withLess({
+  lessLoaderOptions: {},
+  webpack(config, context) {
+    config.plugins.push(UnoCSS({ presets: [presetUno()] }));
+
+    if (context.buildId !== "development") {
+      // * disable filesystem cache for build
+      // * https://github.com/unocss/unocss/issues/419
+      // * https://webpack.js.org/configuration/cache/
+      config.cache = false;
+    }
+
+    return config;
+  },
+  ...nextConfig
+});
