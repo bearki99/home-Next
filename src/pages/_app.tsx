a83baac50b3a40b081629edf8c09135e6204d4fd
bear <424229909@qq.com>
2023-01-16 09:16:16 +0800

    layout设置结束

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import "normalize.css";
 import "@/styles/globals.less";
 import "uno.css";
 import type { AppProps } from "next/app";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import styles from "@/styles/Home.module.less";
-import { Button } from "antd";
-import { useState } from "react";
 
 export default function HomePage() {
-  const [count, setCount] = useState(0);
 
   return (
-    <main>
-      <h1 className="text-center">unocss</h1>
-      <h1 className={styles.title}>Css Modules</h1>
-      <Button onClick={() => setCount(count + 1)}>{count}++</Button>
+    <main className={styles.title}>
+      main-page
     </main>
   );
 }
