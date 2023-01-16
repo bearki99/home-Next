7f7420990d04fd594060e26501c657f765960a03
bear <424229909@qq.com>
2023-01-16 09:49:57 +0800

    redux引入结束

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import "normalize.css";
+import "antd/dist/reset.css";
 import "@/styles/globals.less";
 import "uno.css";
+
+import { Provider } from "react-redux";
+import wrapper from "@/store";
 import type { AppProps } from "next/app";
+
 import Layout from "@/components/layout";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, ...rest }: AppProps) {
+  // Redux接入
+  const { store, props } = wrapper.useWrappedStore(rest);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...props.pageProps} />
+      </Layout>
+    </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import styles from "@/styles/Home.module.less";
+import { useSelector, useDispatch } from "react-redux";
+import type { IAppDispatch, IAppState } from "@/store";
+import { incrementAction } from "@/store/modules/home";
+import { Button } from "antd";
 
 export default function HomePage() {
-
+  const dispatch = useDispatch<IAppDispatch>();
+  const { counter } = useSelector((state: IAppState) => ({
+    counter: state.home.counter,
+  }));
+  function changeCounter() {
+    const newCounter = counter + 1;
+    dispatch(incrementAction(newCounter));
+  }
   return (
-    <main className={styles.title}>
-      main-page
-    </main>
+    <>
+      <main className={styles.title}>main-page</main>
+      <div className={styles.counter}>Redux-counter: {counter}</div>
+      <Button type="primary" onClick={changeCounter} className={styles.btn}>
+        +1
+      </Button>
+    </>
   );
 }
