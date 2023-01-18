3baf49bd12173a67cdfa21d9aafc22cc77622c24
bear <424229909@qq.com>
2023-01-18 09:44:41 +0800

    hover实现

--- src/components/subheader/index.tsx
+++ src/components/subheader/index.tsx
@@ -1,10 +1,11 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useCallback, useState } from "react";
 import { memo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./styles.module.less";
 import Link from "next/link";
 import classNames from "classnames";
 import { changeinitialSubIndexAction } from "../header/store";
+import { debounce } from "lodash";
 interface IProps {
   children?: ReactNode;
 }
@@ -15,6 +16,15 @@ const SubHeader: React.FC<IProps> = () => {
   }));
   homeTags = homeTags.slice(0, 1);
   const dispatch = useDispatch();
+  const [showSubTags, setShowSubTags] = useState(false);
+  const showTag = useCallback(
+    debounce(() => setShowSubTags(true), 500),
+    []
+  );
+  const hideTag = useCallback(
+    debounce(() => setShowSubTags(false), 300),
+    []
+  );
   return (
     <div className={styles.container}>
       <div className={styles.headerList}>
@@ -34,16 +44,34 @@ const SubHeader: React.FC<IProps> = () => {
                     onClick={() => {
                       dispatch(changeinitialSubIndexAction(index));
                     }}
+                    onMouseEnter={showTag}
+                    onMouseLeave={hideTag}
                   >
                     <span className={styles.text}>
                       {item.name.substring(0, 2)}
                     </span>
                   </Link>
                   {item.labels && (
-                    <div className={styles.hoverItem}>
-                      {item.labels.map((item: any) => {
-                        return <Link key={item.id} href={item.id} className={styles.gotoItem}>{item.label}</Link>;
-                      })}
+                    <div
+                      className={classNames(
+                        {
+                          [styles.show]: showSubTags === true,
+                          [styles.hide]: showSubTags === false,
+                        },
+                        [styles.hoverItem], [styles.forceHidden]
+                      )}
+                    >
+                      <ul className={styles.tagList}>
+                        {item.labels.map((item: any) => {
+                          return (
+                            <li key={item.id} className={styles.tag}>
+                              <Link href={item.id} className={styles.gotoItem}>
+                                {item.label.substring(0, 4)}
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </ul>
                     </div>
                   )}
                 </div>
