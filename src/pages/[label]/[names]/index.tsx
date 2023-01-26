95295e80e4337c395a25cce3e2b8c7c42b3392c8
bear <424229909@qq.com>
2023-01-26 22:13:33 +0800

    广告修改

--- src/pages/[label]/[names]/index.tsx
+++ src/pages/[label]/[names]/index.tsx
@@ -113,16 +113,6 @@ const SubContent: React.FC<IProps> = (props) => {
     <>
       <Head>
         <title>{myIndex && nameArr && nameArr[myIndex]} - 掘金</title>
-        <meta
-          name="description"
-          data-n-head="ssr"
-          content="掘金是面向全球中文开发者的技术内容分享与交流平台。我们通过技术文章、沸点、课程、直播等产品和服务，打造一个激发开发者创作灵感，激励开发者沉淀分享，陪伴开发者成长的综合类技术社区。"
-        ></meta>
-        <meta
-          name="keywords"
-          data-n-head="ssr"
-          content="掘金,稀土,Vue.js,前端面试题,Kotlin,ReactNative,Python"
-        ></meta>
       </Head>
       {/* 次导航栏 */}
       {names && <Subheader homeTags={homeTags} />}
--- src/service/request.ts
+++ src/service/request.ts
@@ -1,12 +1,12 @@
-// import request from "@/service/index"; //正式数据时候用这个，下面记得换成request
+import request from "@/service/index"; //正式数据时候用这个，下面记得换成request
 
-import { testRequest } from "@/service/index"; //pr时的测试
+// import { testRequest } from "@/service/index"; //pr时的测试
 
 import { IArticleListRequest } from "@/assets/interface/article";
 
 export function getArticleListApi(data: IArticleListRequest) {
-  return testRequest.get("/api/getArticles", data);
+  return request.get("/api/getArticles", data);
 }
 export function getAuthorListApi() {
-  return testRequest.get("/api/authorsRank");
+  return request.get("/api/authorsRank");
 }
