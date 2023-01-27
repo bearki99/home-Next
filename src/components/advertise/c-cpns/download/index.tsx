51638dd2bc84c8b0d31ffc4d91c81d6989bccca1
bear <424229909@qq.com>
2023-01-27 22:48:05 +0800

    merge-+ad+href+image

--- src/components/advertise/c-cpns/download/index.tsx
+++ src/components/advertise/c-cpns/download/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { memo } from "react";
 import styles from "./style.module.less";
 import Link from "next/link";
+import Image from "next/image";
 interface IProps {
   children?: ReactNode;
 }
@@ -11,7 +12,13 @@ const Download: React.FC<IProps> = () => {
   return (
     <div className={styles.container}>
       <Link href="/app" className={styles.link}>
-        <img src={iconUrl} alt="下载" className={styles.downloadImg} />
+        <Image
+          src={iconUrl}
+          alt="下载"
+          className={styles.downloadImg}
+          width={100}
+          height={100}
+        />
         <div className={styles.right}>
           <div className={styles.headLine}>下载稀土掘金APP</div>
           <div className={styles.desc}>一个帮助开发者成长的社区</div>
