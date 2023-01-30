b7acd54cfd099490347df498bcdbf88c53a7a925
beary <114394346+bearki99@users.noreply.github.com>
2023-01-30 12:11:35 +0800

    feat: 黑暗模式持久化 (#12)

--- src/components/advertise/c-cpns/ad-item/index.tsx
+++ src/components/advertise/c-cpns/ad-item/index.tsx
@@ -30,7 +30,7 @@ const ADItem: React.FC<IProps> = (props) => {
   }, []);
   return (
     <div className={classNames([styles.adContent])}>
-      <Link href={"/article/" + id}>
+      <Link href={"/article/" + id} target="_blank">
         <Image
           src={image}
           alt="广告"
--- src/components/dark-btn/index.tsx
+++ src/components/dark-btn/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { ReactNode, useEffect, useState } from "react";
 import { memo } from "react";
 import styles from "./style.module.less";
@@ -9,20 +8,29 @@ interface IProps {
 
 const DarkBtn: React.FC<IProps> = () => {
   const [isDark, setIsDark] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const handleClick = () => {
+    if (!isDark) localStorage.setItem("myDark", "1");
+    else localStorage.setItem("myDark", "0");
+    setIsDark(!isDark);
+  };
   useEffect(() => {
     if (typeof window !== "undefined") {
-      if (isDark && document) document.body.className = "dark";
-      else document.body.className = "light";
+      if (document && localStorage.getItem("myDark") == null) {
+        document.body.className = "light";
+        setFlag(false);
+      } else if (localStorage.getItem("myDark") == "1") {
+        document.body.className = "dark";
+        setFlag(true);
+      } else {
+        document.body.className = "light";
+        setFlag(false);
+      }
     }
   }, [isDark]);
   return (
-    <div
-      className={styles.darkContent}
-      onClick={() => {
-        setIsDark(!isDark);
-      }}
-    >
-      {!isDark ? (
+    <div className={styles.darkContent} onClick={handleClick}>
+      {!isDark && !flag ? (
         <Button className={styles.darkBtn}>黑暗模式</Button>
       ) : (
         <Button className={styles.lightBtn}>白天模式</Button>
